3a0f87dc6094c5b3f2953fa968aa8435a185bbfd
yuchen <yl6448@nyu.edu>
2020-10-28 13:36:46 -0700

    Add post validations and test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
     has_rich_text :body
+    validates :title, :body, presence: true
+    validates :title, uniqueness: true
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-
   test 'has timestamp for creation' do
     puts posts(:one).created_at
     assert posts(:one).created_at?
+  end
+
+  test 'name has to be unique' do
+    one = posts(:one)
+    post = Post.new(title: one.title, body: 'test')
+    assert post.invalid?
+  end
+
+  test 'need both title and body' do
+    no_title = Post.new(body: 'test')
+    assert no_title.invalid?
+
+    no_body = Post.new(title: 'test')
+    assert no_body.invalid?
   end
 end
