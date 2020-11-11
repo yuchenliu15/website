d62c220f150982ad6568e1a06b72d45bb87d3421
yuchen <yl6448@nyu.edu>
2020-11-10 16:34:58 -0800

    Fix tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { name: @user.name, password: 'secret', password_confirmation: 'secret' } }
+      post users_url, params: { user: { name: 'different', password: 'secret', password_confirmation: 'secret' } }
     end
 
     assert_redirected_to user_url(User.last)
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
   test 'has timestamp for creation' do
-    puts posts(:one).created_at
     assert posts(:one).created_at?
   end
 
